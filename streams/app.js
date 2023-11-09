c5a8aa4b4635c5fb676213339d5ea39bf1333beb
LIGHTHINGS <dareomotayo32@gmail.com>
2023-11-09 04:07:32 +0100

    wip✨

--- streams/app.js
+++ streams/app.js
@@ -1,20 +1,32 @@
-const fs = require('node:fs');
-
-
-const deleteFile = () => {
-    fs.open('./come.txt','r', (err, file) => {
-        if (err) {
-            fs.writeFileSync('come.txt', 'Hello world.. This is stream from Node', 'utf-8');
-        }
-        else if (file) {
-            fs.unlinkSync('come.txt')
-            console.log('./come.txt deleted because it already exist')
-        } else {
-            console.log('Something wrong happened')
-        }
-    })
-}
-
-deleteFile()
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = require("fs");
+var deleteFile = function (Data) {
+    if (Data.includes('.js')) {
+        (0, fs_1.open)(Data, 'r', function (err, file) {
+            if (err) {
+                (0, fs_1.writeFileSync)(Data, "const fileName = '".concat(Data, "'"), 'utf-8');
+            }
+            else if (file) {
+                (0, fs_1.unlinkSync)(Data);
+                console.log("./".concat(Data, " deleted because it already exist"));
+            }
+        });
+    }
+    else {
+        (0, fs_1.open)(Data, 'r', function (err, file) {
+            if (err) {
+                (0, fs_1.writeFileSync)(Data, 'Hello world.. This is stream from Node', 'utf-8');
+            }
+            else if (file) {
+                (0, fs_1.unlinkSync)(Data);
+                console.log("./".concat(Data, " deleted because it already exist"));
+            }
+            else {
+                console.log('Something wrong happened');
+            }
+        });
+    }
+};
+deleteFile('Dare.js');
 // fs.unlinkSync('come.txt')
